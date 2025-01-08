f0b2554dcd87fc64eb4608aba000ceb7511f7ecf
sushanth7781 <sushanth.kk2004@gmail.com>
2025-01-08 20:06:00 +0530

    python file

--- link.py
+++ link.py
@@ -6,6 +6,10 @@ node1=node(5)
 node2=node(4)
 node3=node(6)
 node4=node(7)
+node1.next=node2
+node2.next=node3
+node3.next=node4
+
 currentnode=node1
 while currentnode:
     print(currentnode.data,end='->')
